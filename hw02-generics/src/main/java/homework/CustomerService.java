a40b4c38b820d83aa8b2c4dfd46b9e6527474f6d
DM0311 <karapuzoff90@mail.ru>
2025-08-06 10:38:12 +0300

    Hw02 generics (#2)
    * hw02-generics/collections
    
    * hw02-generics/collections
    
    * hw02-generics/collections
    
    ---------
    
    Co-authored-by: poltev_dv <poltev_dv@nlmk.com>

---
+++ hw02-generics/src/main/java/homework/CustomerService.java
@@ -0,0 +1,46 @@
+package homework;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
+
+public class CustomerService {
+
+    private NavigableMap<Customer, String> dataByCustomer = new TreeMap<>(Comparator.comparing(Customer::getScores));
+
+    // важно подобрать подходящую Map-у, посмотрите на редко используемые методы, они тут полезны
+
+    public Map.Entry<Customer, String> getSmallest() {
+
+        // Возможно, чтобы реализовать этот метод, потребуется посмотреть как Map.Entry сделан в jdk
+        Map.Entry<Customer, String> originalEntry = dataByCustomer.firstEntry();
+        if (originalEntry == null) {
+            return null;
+        }
+        return Map.entry(
+                new Customer(
+                        originalEntry.getKey().getId(),
+                        originalEntry.getKey().getName(),
+                        originalEntry.getKey().getScores()),
+                originalEntry.getValue());
+    }
+
+    public Map.Entry<Customer, String> getNext(Customer customer) {
+        Map.Entry<Customer, String> originalEntry = dataByCustomer.higherEntry(customer);
+
+        if (originalEntry == null) {
+            return null;
+        }
+        return Map.entry(
+                new Customer(
+                        originalEntry.getKey().getId(),
+                        originalEntry.getKey().getName(),
+                        originalEntry.getKey().getScores()),
+                originalEntry.getValue());
+    }
+
+    public void add(Customer customer, String data) {
+        dataByCustomer.put(customer, data);
+    }
+}
