8cbe2d6d968140ef00c5152ad9e63b6ed1b6180c
poltev_dv <poltev_dv@nlmk.com>
2025-09-09 21:08:58 +0300

    hw05-bytecode

---
+++ hw05-bytecode/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("java")
+}
+
+group = "ru.otus.java.pro"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation ("ch.qos.logback:logback-classic")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ hw05-bytecode/src/main/java/ru/otus/java/pro/source/Calculation.java
@@ -0,0 +1,9 @@
+package ru.otus.java.pro.source;
+
+public interface Calculation {
+    public void calculation(int param1);
+
+    public void calculation(int param1, int param2);
+
+    public void calculation(int param1, int param2, String param3);
+}
