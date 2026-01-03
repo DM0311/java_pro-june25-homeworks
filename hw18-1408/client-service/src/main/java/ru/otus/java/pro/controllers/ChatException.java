97ad45ac78d09e6ed8302df1eca4d686a785508a
poltev_dv <poltev_dv@nlmk.com>
2026-01-03 17:34:11 +0300

    hw18-1408

---
+++ hw18-1408/client-service/src/main/java/ru/otus/java/pro/controllers/ChatException.java
@@ -0,0 +1,7 @@
+package ru.otus.java.pro.controllers;
+
+public class ChatException extends RuntimeException {
+    public ChatException(String message) {
+        super(message);
+    }
+}
---
+++ hw18-1408/client-service/src/main/java/ru/otus/java/pro/domain/Message.java
@@ -0,0 +1,3 @@
+package ru.otus.java.pro.domain;
+
+public record Message(String messageStr) {}
